c954a32980bfcce091468b2f494d2b65b4f082d9
Christalpena <christalperez0@gmail.com>
2025-01-15 00:22:36 -0400

    extra exercice

---
+++ JuniorLevel/POO/extra.js
@@ -0,0 +1,9 @@
+/**
+ * Crear una funcion que tome una lista de numeros repetidos, la funcion debe de encargarse de retornar la lista sin numeros repetidos
+ */
+
+let numbers = [1,2,1,14,2,15,16,2,14,14]
+
+let result = numbers.filter((number,index) => !numbers.slice(index+1).includes(number))
+
+document.write(result)
