c243038c8688f22fc88b4ce12bc23cc9196057e7
Christalpena <christalperez0@gmail.com>
2025-01-22 22:56:53 -0400

    New

---
+++ JuniorLevel/Extra/extra2.js
@@ -0,0 +1,6 @@
+// escribe tu función acá
+
+// código de prueba
+let words = ["beta", "alfa", "Arbol", "gama"]
+let result = words.filter((word) => word[0] == 'a' || word[0] == "A")
+console.log(result) // ["alfa", "Arbol"]
